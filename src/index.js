b683567ea562ffadb4a77d6238cc8ba329e03cb6
pauldropz <pauldropz@gmail.com>
2022-09-23 19:41:45 +0100

    all username added to card

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ root.render(
      <div> 
          <Card id= {robots[0].id} name={robots [0].name} username={robots[0].username} email={robots[0].email}/>
          <Card id= {robots[1].id} name={robots [1].name} username={robots[1].username} email={robots[1].email}/>
-         <Card id= {robots[2].id} name={robots [2].name} email={robots[2].email}/>
-         <Card id= {robots[3].id} name={robots [3].name} email={robots[3].email}/>
-         <Card id= {robots[4].id} name={robots [4].name} email={robots[4].email}/>
-         <Card id= {robots[5].id} name= {robots[5].name} email={robots[5].email}/>
+         <Card id= {robots[2].id} name={robots [2].name} username={robots[2].username} email={robots[2].email}/>
+         <Card id= {robots[3].id} name={robots [3].name} username={robots[3].username} email={robots[3].email}/>
+         <Card id= {robots[4].id} name={robots [4].name} username={robots[4].username} email={robots[4].email}/>
+         <Card id= {robots[5].id} name= {robots[5].name} username={robots[5].username} email={robots[5].email}/>
 
 
 
