59bdf62cc59744353b80568e648724973b4dba92
pauldropz <pauldropz@gmail.com>
2022-09-23 23:11:17 +0100

    CardList importation in index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Card from './Card';
+import CardList from "./CardList"
 import reportWebVitals from './reportWebVitals';
 import { robots } from './robots';
 
