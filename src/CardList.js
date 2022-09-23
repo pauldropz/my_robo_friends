0ade7b911c8cf12553dd31c18fc82a8327379803
pauldropz <pauldropz@gmail.com>
2022-09-23 23:51:05 +0100

    unique key 'key= 'i' creation

--- src/CardList.js
+++ src/CardList.js
@@ -4,7 +4,7 @@ import Card from "./card";
 const CardList = ( {robots}) => {
     const cardComponent = robots.map (user, i{
         return 
-        <Card id= {robots[0].id} name={robots [0].name} username={robots[0].username} email={robots[0].email}/>
+        <Card key="i" id= {robots[0].id} name={robots [0].name} username={robots[0].username} email={robots[0].email}/>
     })
         return (
 
