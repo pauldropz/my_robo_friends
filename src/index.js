db498e9ae6f7a7ed99db7fe5497d490b4ab99ecb
pauldropz <pauldropz@gmail.com>
2022-09-23 23:29:34 +0100

    Eport included for CardList.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-      <CardList />
+      <CardList robots= {robots}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
