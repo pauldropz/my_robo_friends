a094f787a2aaff5692dc8f3aa7ece99758fafc08
pauldropz <pauldropz@gmail.com>
2022-09-23 23:08:42 +0100

    CardList.js created

--- src/Robots.js
+++ src/Robots.js
@@ -1,15 +1,40 @@
 export const robots = [
     {
             id: 1,
-            name: "Bobby Ray";
-            email: "Badess@gmail.com";
+            name: "Bobby Ray",
+            username: "Jonnycage",
+            email: "Badess@gmail.com"
     },
-    
+    {
+            id: 2,
+            name: "Filty Takatans",
+            username: "Baraka",
+            email: "Barakat@ymail.com"
 
+    },
+    {
+            id: 3,
+            name: "Hanzo Hasashi",
+            username: "Scorpion",
+            email: "Shirairaiyu@gmail.com"
 
-
-
-
-
-
-]
+    },
+    {
+            id: 4,
+            name: "Jax Brigx",
+            username: "Jaxx",
+            email: "Jaxbriqx@gmail.com"
+    },
+    {
+            id: 5,
+            name: "Sonya Blade",
+            username: "Sonya",
+            email: "Sonyablade@ymail.com"
+    },
+    {
+            id: 6,
+            name: "Kwa Lee",
+            username: "Sub-zero",
+            email: "Linkuel@gmail.com"
+    }
+];
--- src/card.js
+++ src/card.js
@@ -2,14 +2,18 @@ import React from "react";
 import "./Card.css";
 import robot from "./assets/images/robot face1.jpg";
 
-const Card = () => {
+const Card = ({name, email, username, id} ) => {
     return (
         <div className="khan"> 
 
             <img src= {robot} width="250rem" alt="Emperor"/>
             <div className="emperor">
-                <h1>Kotal Khan</h1>
-                <p>KotalKhan@gmail.com</p>
+                {/* <h1>{props.name}</h1>
+                <p>{props.username}</p>
+                <p>{props.email}</p> */}
+                <h1>{name}</h1>
+                <h1>{email}</h1>
+                <h1>{username}</h1>
             </div>
 
         </div>
