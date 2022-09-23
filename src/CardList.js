6249bdb88beaa0672021b557c41601e4fc2f0955
pauldropz <pauldropz@gmail.com>
2022-09-23 23:40:55 +0100

    CardList maping

--- src/CardList.js
+++ src/CardList.js
@@ -2,6 +2,10 @@ import React from "react";
 import Card from "./card";
 
 const CardList = ( {robots}) => {
+    const cardComponentd = robots.map (user =>{
+         <Card id= {robots[0].id} name={robots [0].name} username={robots[0].username} email={robots[0].email}/>
+         return 
+    })
         return (
 
                 <div>
