6e0de37ca328f770bb88ea0e184d887dbe8cf5f0
pauldropz <pauldropz@gmail.com>
2022-09-24 17:35:21 +0100

    Cardlist late adjustment

--- src/App.js
+++ src/App.js
@@ -1,3 +1,3 @@
 import React from "react";
 
-// import CardList from "./CardList";
+import CardList from "./CardList";
--- src/CardList.js
+++ src/CardList.js
@@ -5,7 +5,8 @@ const CardList = () => {
         return (
 
             <>
-                 <Card />
+                
+                
             
             </>
 
