ecc94072609bda5d4307af5ae3ea9ef976454668
pauldropz <pauldropz@gmail.com>
2022-11-29 21:05:51 +0100

    zagz

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
             <>
             <div>
 
-                    {CardList}
+         
 
                     </div>
             </>
---
+++ src/card.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Card.css";
+import robot from "./assets/images/robot face1.jpg";
+
+const Card = ({name, email, username} ) => {
+    return (
+        <div className="khan"> 
+
+            <img src= {robot} width="250rem" alt="Emperor"/>
+            <div className="emperor">
+                <h1>{name}</h1>
+                <h1>{username}</h1>
+                <h1>{email}</h1>
+    
+
+
+            </div>
+
+        </div>
+
+
+    )
+    }
+export default Card;
